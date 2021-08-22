0e2a5b03b56b608a8860da40024166f75dc06c4d
MasatoMakino <unagiinu.g@gmail.com>
2021-08-22 21:28:10 +0900

    update : devDependencies.

--- gulpfile.ts
+++ gulpfile.ts
@@ -4,7 +4,7 @@ const doc = require("gulptask-tsdoc").generateTask();
 const server = require("gulptask-dev-server").generateTask("./docs/demo");
 const { bundleDemo, watchDemo } = require("gulptask-demo-page").generateTasks({
   externalScripts: [],
-  body: `<canvas id="webgl-canvas" width="1920" height="1080"></canvas>`,
+  body: `<canvas id="webgl-canvas" width="1280" height="640"></canvas>`,
 });
 const { tsc, tscClean, watchTsc } = require("gulptask-tsc").generateTasks({
   projects: ["tsconfig.cjs.json", "tsconfig.esm.json"],
